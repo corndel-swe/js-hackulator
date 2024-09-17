0aa8f01396686bb55f4588f978f37554aed97271
Simon Haines <nimon@hotmail.co.uk>
2024-09-17 16:27:58 +0100

    Update gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -2,6 +2,7 @@
  * Works out the greatest common divisor of two numbers
  * Uses the euclidean algorithm: https://www.youtube.com/watch?v=JUzYl1TYMcU
  * e.g. gcd(8, 12) => 4
+ * e.g. gcd(15, 10) => 5
  */
 function gcd(a, b) {
   while (a !== 0) {
